22ad128987539074a918c0dab5f361f82359cc55
Asich <amustafin@crystalspring.kz>
2015-05-07 15:48:16 +0600

    init commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// app.get('/', function(req, res){
+//   res.send('<h1>Hello world</h1>');
+// });
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+
+  console.log('a user connected');
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    //console.log('message: ' + msg);
+    io.emit('chat message', msg);//broadcast to other users
+    
+    //save to mongo
+
+  });
+
+
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
